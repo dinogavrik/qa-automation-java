732e6cb9aa321b50b822c0aaf8b47c7ba3bf5f42
i.gavrikova <i.gavrikova>
2022-04-15 14:48:27 +0300

    feat: added the ability to set multiple messages

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -5,11 +5,11 @@ import static com.tcs.edu.service.MessageService.process;
 
 class Application {
     public static void main(String[] args) {
-        process(MINOR,"Hello world!");
+        process(MINOR,"Hello world!", "Hi!", "Aloha");
         process(REGULAR,"Hello world!");
         process(REGULAR,"Hello world!");
-        process(MINOR,"Hello world!");
-        process(MINOR,"Hello world!");
-        process(MAJOR,"Hello world!");
+        process(MINOR,"Hello world!", "Ola", "Ciao", "Hola!");
+        process(MAJOR,"Hello world!", "Hallo", "Salut", "Guten Tag");
+        process(REGULAR,"Hello world!");
     }
 }
--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -3,6 +3,9 @@ package com.tcs.edu.service;
 import com.tcs.edu.decorator.PaginationDecorator;
 import com.tcs.edu.decorator.Severity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static com.tcs.edu.decorator.SeverityLevelDecorator.mapSeverity;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 import static com.tcs.edu.printer.ConsolePrinter.print;
@@ -13,13 +16,18 @@ import static com.tcs.edu.printer.ConsolePrinter.print;
 public class MessageService {
 
     /**
-     * Method for assembling the output message
+     *Method for assembling the output message
      *
      * @param level message significance level
      * @param message transmitted message
+     * @param messages transmitted messages
      */
-    public static void process (Severity level, String message) {
-        print(PaginationDecorator.decorate(decorate(message) + " " + mapSeverity(level)));
+    public static void process (Severity level, String message, String... messages) {
+        ArrayList<String> allMess = new ArrayList<>(Arrays.asList(messages));
+        allMess.add(0, message);
+        for (String mess : allMess ) {
+            print(PaginationDecorator.decorate(decorate(mess) + " " + mapSeverity(level)));
+        }
     }
 
 
