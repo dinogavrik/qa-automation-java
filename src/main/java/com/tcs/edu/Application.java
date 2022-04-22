a99f0eb6704db62d99faff834d16d4f588f15468
i.gavrikova <i.gavrikova>
2022-04-22 10:27:30 +0300

    fix: ignoring null values

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -6,10 +6,10 @@ import static com.tcs.edu.service.MessageService.process;
 class Application {
     public static void main(String[] args) {
         process(MINOR,"Hello world!", "Hi!", "Aloha");
-        process(REGULAR,"Hello world!");
-        process(REGULAR,"Hello world!");
-        process(MINOR,"Hello world!", "Ola", "Ciao", "Hola!");
-        process(MAJOR,"Hello world!", "Hallo", "Salut", "Guten Tag");
-        process(REGULAR,"Hello world!");
+        process(REGULAR,"Hello world!", null);
+        process(REGULAR,null);
+        process(MINOR,"Hello world!", null, "Ola", "Hola!");
+        process(MAJOR,"Hello world!", "Hallo", null, "Guten Tag");
+        process(REGULAR, null, null);
     }
 }
--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -3,9 +3,7 @@ package com.tcs.edu.service;
 import com.tcs.edu.decorator.PaginationDecorator;
 import com.tcs.edu.decorator.Severity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 import static com.tcs.edu.printer.ConsolePrinter.print;
@@ -15,20 +13,27 @@ import static com.tcs.edu.printer.ConsolePrinter.print;
  */
 public class MessageService {
 
+    public static boolean doubling = true;
+
     /**
-     *Method for assembling the output message
+     * Method for assembling the output message
      *
-     * @param level message significance level
-     * @param message transmitted message
+     * @param level    message significance level
+     * @param message  transmitted message
      * @param messages transmitted messages
      */
-    public static void process (Severity level, String message, String... messages) {
+    public static void process(Severity level, String message, String... messages) {
+        if (messages == null) {
+            messages = new String[]{};
+        }
         List<String> allMess = new ArrayList<>(Arrays.asList(messages));
         allMess.add(0, message);
-        for (String mess : allMess ) {
-            print(PaginationDecorator.decorate(String.format("%s %s",decorate(mess), level.getLevel())));
+        for (String mess : allMess) {
+            if (mess != null) {
+                print(PaginationDecorator.decorate(String.format("%s %s", decorate(mess), level.getLevel())));
+            }
         }
     }
 
-
 }
+
